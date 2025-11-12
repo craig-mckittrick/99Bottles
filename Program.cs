999feb14c24d951e93d552acfc14f373ffca62ed
Craig McKittrick <craig.mckittrick.1@outlook.com>
2025-11-12 09:30:12 -0600

    Add initial implementation of the "99 Bottles of Beer" program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+int bottles = 99;
+for (int i = bottles; i >= 0; i--)
+{
+    switch (i)
+    {
+        case > 1:
+            Console.WriteLine();
+            Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+            Console.WriteLine($"Take one down and pass it around, {--bottles} bottles of beer on the wall.");
+            break;
+        case 1:
+            Console.WriteLine();
+            Console.WriteLine($"{bottles} bottle of beer on the wall, {bottles} bottle of beer.");
+            Console.WriteLine($"Take one down and pass it around, {--bottles} bottles of beer on the wall.");
+            break;
+        case 0:
+            Console.WriteLine();
+            Console.WriteLine("No more bottles of beer...go to the store and buy some more.");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            break;
+    }
+}
+
+
